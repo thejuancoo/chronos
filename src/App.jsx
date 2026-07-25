011e5899ea45a972fe0ced2e8e24e13ed37d4d9a
Juan Cruz <jmcruzo23z@gmail.com>
2026-07-24 18:11:58 -0600

    feat: Agrega el router y las primeras rutas de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Route, Routes } from "react-router"
+import Home from "./views/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        Hola
-      </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </>
   )
 }
