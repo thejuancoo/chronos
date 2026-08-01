78a45cddc5b62c8942623fc787ea1482c077ffaa
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-01 14:52:48 -0600

    feat: Agrega las pantallas principales

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router"
 import Home from "./views/Home"
+import Calendar from "./features/Calendar/Calendar"
+import Dashboard from "./features/Dashboard/Dashboard"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/dashboard/calendar" element={<Calendar/>}/>
       </Routes>
     </>
   )
