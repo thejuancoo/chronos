7ca89de4b2c7059c50ff0c2b05c23e2a534f0ed6
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-01 23:48:50 -0600

    feat: Agrega las rutas al sidebar

--- src/shared/components/layout/Sidebar.jsx
+++ src/shared/components/layout/Sidebar.jsx
@@ -1,6 +1,25 @@
-import { Link } from "react-router"
+import { useState } from "react"
+import { NavLink, Link } from "react-router"
+import { HomeIcon, CalendarIcon } from "@heroicons/react/24/outline"
 
 export default function Sidebar() {
+  const classIcons = "size-5 mx-2 text-gray-700 dark:text-gray-200"
+  const sidebarOptions = [
+    {
+      name: "Inicio",
+      link: "",
+      icon: <HomeIcon className={classIcons}/>,
+      active: true
+    },
+    {
+      name: "Calendario",
+      link: "calendar",
+      icon: <CalendarIcon className={classIcons}/>,
+      active: true
+    }
+  ]
+
+
   return (
     <aside className="hidden md:flex flex-col fixed top-0 left-0 w-62.5 h-screen border border-gray-200 bg-white">
       <div className="h-14 flex items-center border-b border-gray-200">
@@ -13,9 +32,21 @@ export default function Sidebar() {
       </div>
 
       <nav className="flex-1 overflow-y-auto bg-gray-100">
-        <div>
-        </div>
-        </nav>
+        {
+          sidebarOptions.filter(nav => nav.active != false).map(nav => (
+            <NavLink
+              to={`/dashboard/${nav.link}`} end
+              key={nav.name}
+              className={({ isActive }) =>
+                `flex flex-row justify-start items-center px-2 py-1 cursor-pointer 
+                                    ${isActive ? "bg-gray-200 dark:bg-gray-800 dark:text-white" : "hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-800"}`}
+            >
+              {nav.icon}
+              <p className="text-lg">{nav.name}</p>
+            </NavLink>
+          ))
+        }
+      </nav>
     </aside>
   )
 }
