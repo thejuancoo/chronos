0eee028668ceffff000d5171a10e323b339df48a
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-03 22:33:17 -0600

    feat: Agrega los interceptores en la respuesta

--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,15 @@ import axios from "axios";
 const apiInstance = axios.create({
     baseURL: import.meta.env.API_URL
 })
+
+apiInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
+
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+})
+
+export default apiInstance
