5857fccae6ef93a06e6bf8258a84b72f069bdadd
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-04 12:57:20 -0600

    feat: Renombra las variables de entorno

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-    baseURL: import.meta.env.API_URL
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 apiInstance.interceptors.request.use((config) => {
