653dd951a9f45a6c5c967aa6bc2f1ecbe216439d
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-06 16:23:21 -0600

    style: Agrega un boton para cerrar la sesion

--- src/shared/components/layout/Navbar.jsx
+++ src/shared/components/layout/Navbar.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 h-14 flex items-center justify-between border-b border-gray-200 bg-gray-100 px-4">
-        Navbar
+    <nav className="sticky top-0 z-50 h-14 flex items-center justify-between border-b border-gray-200 bg-white px-4">
+      <div>
+
+      </div>
+      <div>
+        <button
+          onClick={() => {}}
+          className="hover:bg-red-100 hover:text-red-500 p-2 rounded-md border border-gray-200"
+        >Cerrar Sesión</button>
+      </div>
     </nav>
   )
 }
