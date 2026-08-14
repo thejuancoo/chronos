3ade801b4a9bdcf3a2f7c25ee1c3662275d21edf
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-13 19:02:34 -0600

    feat: Muestra los elementos faltantes de los eventos en los cards

--- src/shared/components/layout/DialogShowEvent.jsx
+++ src/shared/components/layout/DialogShowEvent.jsx
@@ -15,12 +15,23 @@ export default function DialogShowEvent({isModalEventOpen, setIsModalEventOpen, 
         transition
         className="fixed overflow-auto py-8 inset-0 flex w-screen items-center justify-center bg-black/40 transition duration-300 ease-out data-closed:opacity-0"
     >
-        <DialogPanel className="max-w-lg bg-white p-8">
-            <DialogTitle className="font-medium text-xl">Eventos del dia</DialogTitle>
-            {selectedDay?.transactions?.map((event, index) => (
-                <div key={event.id}>{event.title}</div>
-            ))}
-        </DialogPanel>
-    </Dialog>
+        <DialogPanel className="w-[90vw] max-w-lg rounded-lg sm:max-w-125 md:pt-6 bg-white p-8">
+              <DialogTitle className="font-medium text-xl">Eventos del dia</DialogTitle>
+                {selectedDay?.transactions?.length > 0 ? (
+                    selectedDay.transactions.map((event) => (
+                        <div key={event.id}>
+                            <h2 className="font-semibold">{event.title}</h2>
+                            <p>{event.description}</p>
+                            <p>{event.date}</p>
+                            <p>{event.time}</p>
+                        </div>
+                    ))
+                ) : (
+                    <div className="text-center p-4">
+                        <p>No hay eventos disponibles</p>
+                    </div>)
+                }
+          </DialogPanel>
+      </Dialog>
   )
 }
