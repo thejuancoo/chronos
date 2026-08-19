a4134fbe795aab45c90e1ee47c53f9eb5845ff33
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-19 01:01:42 -0600

    style: Modifica los estilos de los eventos que hay en un solo dia

--- src/shared/components/layout/DialogShowEvent.jsx
+++ src/shared/components/layout/DialogShowEvent.jsx
@@ -5,6 +5,7 @@ import {
     DialogPanel,
     Description
 } from "@headlessui/react"
+import { PlusIcon } from "@heroicons/react/24/outline"
 
 export default function DialogShowEvent({isModalEventOpen, setIsModalEventOpen, selectedDay, setSelectedDay}) {
     console.log(selectedDay)
@@ -16,22 +17,36 @@ export default function DialogShowEvent({isModalEventOpen, setIsModalEventOpen, 
         className="fixed overflow-auto py-8 inset-0 flex w-screen items-center justify-center bg-black/40 transition duration-300 ease-out data-closed:opacity-0"
     >
         <DialogPanel className="w-[90vw] max-w-lg rounded-lg sm:max-w-125 md:pt-6 bg-white p-8">
-              <DialogTitle className="font-medium text-xl">Eventos del dia</DialogTitle>
-                {selectedDay?.transactions?.length > 0 ? (
-                    selectedDay.transactions.map((event) => (
-                        <div key={event.id}>
+            <DialogTitle className="font-medium text-xl">Eventos del dia</DialogTitle>
+            {selectedDay?.transactions?.length > 0 ? (
+                selectedDay.transactions.map((event) => (
+                    <button
+                        key={event.id}
+                        className="flex justify-between items-center w-full border border-gray-200 rounded-lg p-4 mt-2 hover:bg-gray-50"
+                    >
+                        <div className="">
                             <h2 className="font-semibold">{event.title}</h2>
                             <p>{event.description}</p>
                             <p>{event.date}</p>
-                            <p>{event.time}</p>
                         </div>
-                    ))
-                ) : (
-                    <div className="text-center p-4">
-                        <p>No hay eventos disponibles</p>
-                    </div>)
-                }
-          </DialogPanel>
-      </Dialog>
+                        <div>
+                            <p className="text-gray-500">{event.time}</p>
+                        </div>
+                    </button>
+                ))
+            ) : (
+                <div className="text-center p-4">
+                    <p>No hay eventos disponibles</p>
+                </div>)
+            }
+            <button
+                className="flex justify-center items-center py-1 rounded-lg mt-3 text-center w-full border border-gray-200 hover:bg-gray-50"
+                onClick={() => {}}
+            >
+                <PlusIcon className="size-4 mr-1"/>
+                Agregar nuevo evento este dia
+            </button>
+        </DialogPanel>
+    </Dialog>
   )
 }
