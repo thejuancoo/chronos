d921fd78aa30dbcadb8ce6919c720cc36302f7d6
Juan Cruz <jmcruzo23z@gmail.com>
2026-08-29 19:03:29 -0600

    feat: Agrega la vista de registro

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,12 +5,14 @@ import Dashboard from "../features/Dashboard/Dashboard"
 import DashboardLayout from "../shared/components/layout/DashboardLayout"
 
 import Login from "../features/auth/Pages/Login"
+import Register from "../features/auth/Pages/Register"
 
 function Router() {
   return (
     <Routes>
       {/* <Route path="/" element={<Home />} /> */}
       <Route path="/" element={<Login/>}/>
+      <Route path="/signin" element={<Register/>}/>
 
       <Route element={<DashboardLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
