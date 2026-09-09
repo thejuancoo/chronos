9984386be23a8c2e36ba1dc17459cc6b5cff12c3
Juan Cruz <jmcruzo23z@gmail.com>
2026-09-09 13:09:24 -0600

    feat: Crea el modal para editar un evento

---
+++ src/features/Calendar/components/DialogEditEvent.jsx
@@ -0,0 +1,134 @@
+import { useEffect } from "react"
+import { useForm } from "react-hook-form"
+import { useEvents } from "../../../shared/context/EventContext"
+import { 
+    Dialog,
+    Description, 
+    DialogPanel,
+    DialogTitle
+} from "@headlessui/react"
+
+export default function DialogEditEvent({event, isDialogEditEventOpen, setIsDialogEditEventOpen}) {
+    const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm()
+
+    const { editEvent } = useEvents()
+
+    useEffect(() => {
+        if (event) {
+            reset({
+                title_event: event.title_event,
+                description_event: event.description_event,
+                date_event: event.date_event,
+                time_event: event.time_event
+            })
+        }
+    }, [event, reset])
+
+    const onSubmit = async (data) => {
+        try {
+            const result = await editEvent(event.id_event, data)
+            console.log(result)
+            reset()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <Dialog
+            open={isDialogEditEventOpen}
+            onClose={() => setIsDialogEditEventOpen(false)}
+            transition
+            className="fixed overflow-auto inset-0 flex w-screen items-center justify-center bg-black/40 transition duration-300 ease-out data-closed:opacity-0"
+        >
+            <DialogPanel className="w-[90vw] max-w-lg rounded-lg sm:max-w-125 md:pt-6 bg-white py-2 px-6">
+                <DialogTitle className="font-medium text-xl">Modifica un Evento</DialogTitle>
+                <Description className="text-sm text-gray-500">Edita o elimina un evento o dia programado</Description>
+                <div className="py-4">
+                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-1 mt-2">
+                        <div>
+                            <label className="font-medium">Título</label>
+                            <input
+                                className="w-full border border-gray-200 p-2 rounded-md"
+                                placeholder="Ej. Boda"
+                                type="text"
+                                name="title_event"
+                                {...register("title_event")}
+                            />
+                        </div>
+
+                        <div>
+                            <label className="font-medium">Descripcion</label>
+                            <textarea
+                                placeholder="Ej. Terminar de organizar"
+                                className="p-2 w-full h-24 border border-gray-200"
+                                name="description_event"
+                                {...register("description_event")}
+                            />
+                        </div>
+
+                        <div>
+                            <label className="font-medium">Fecha</label>
+                            <input
+                                className="w-full border border-gray-200 p-2 rounded-md"
+                                placeholder="Ej. Boda"
+                                type="date"
+                                name="date_event"
+                                {...register("date_event")}
+                            />
+                        </div>
+
+                        <div>
+                            <label className="font-medium">Hora</label>
+                            <input
+                                className="w-full border border-gray-200 p-2 rounded-md"
+                                placeholder="Ej. Boda"
+                                type="time"
+                                name="time_event"
+                                {...register("time_event")}
+                            />
+                        </div>
+
+                        <div className="flex justify-between mt-4 gap-2">
+                            <button
+                                className="border-2 border-red-300 px-3 py-1 rounded-lg text-red-500 bg-red-100 hover:bg-red-200"
+                                type="submit"
+                                onClick={() => {
+                                    console.log('Eliminando')
+                                    setIsDialogEditEventOpen(false)
+                                }}
+                            >
+                                Eliminar
+                            </button>
+                            <div className="flex gap-3">
+                                <button
+                                    className="border border-gray-200 rounded-lg px-3 py-1 hover:bg-gray-200 hover:cursor-pointer"
+                                    type="button"
+                                    onClick={() => {
+                                        setIsDialogEditEventOpen(false)
+                                        reset()
+                                    }}
+                                >
+                                    Cerrar
+                                </button>
+                                <button
+                                    className="border border-gray-200 rounded-lg px-3 text-gray-100 bg-blue-600 hover:cursor-pointer hover:bg-blue-700"
+                                    type="submit"
+                                    onClick={() => {
+                                        setIsDialogEditEventOpen(false)
+                                    }}
+                                >
+                                    Guardar
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </DialogPanel>
+        </Dialog>
+    )
+}
